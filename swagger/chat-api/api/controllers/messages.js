45fc32afee960ca5e30fe8a2bcc625a53a5c8e3b
Constantine <const@thething.suse>
2017-12-05 00:58:22 +0400

    message.create method

--- swagger/chat-api/api/controllers/messages.js
+++ swagger/chat-api/api/controllers/messages.js
@@ -1,11 +1,18 @@
 'use strict';
 
 module.exports = {
-  all
+  all,
+  create
 };
+
 
 const MESSAGES = [ { user: 'Alice', text: 'hello' }, { user: 'Bob', text: 'hi' } ];
 
 function all(req, res) {
+   res.json(MESSAGES);
+}
+
+function create(req, res) {
+  MESSAGES.unshift(req.swagger.params.message.value);
   res.json(MESSAGES);
 }
