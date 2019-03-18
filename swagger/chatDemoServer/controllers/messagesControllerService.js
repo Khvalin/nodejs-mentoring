9a53db180dbf2c4b8d1776706ea3dc54e17c1b02
Constantine <khvalin@gmail.com>
2019-03-17 21:48:32 -0700

    messages service implementation

--- swagger/chatDemoServer/controllers/messagesControllerService.js
+++ swagger/chatDemoServer/controllers/messagesControllerService.js
@@ -1,13 +1,15 @@
 'use strict'
 
+const CHAT_MESSAGES = [ { user: 'Alice', text: 'hello' }, { user: 'Bob', text: 'hi' } ]
+
 module.exports.createPost = function createPost(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for createPost'
-  });
-};
+  // Body parameter in v3 has no name ( https://github.com/OAI/OpenAPI-Specification/issues/1643 )
+  const { user, text } = req.undefined.value
+  CHAT_MESSAGES.unshift({ user, text })
+
+  res.send(CHAT_MESSAGES)
+}
 
 module.exports.allGet = function allGet(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for allGet'
-  });
-};
+  res.send(CHAT_MESSAGES)
+}
